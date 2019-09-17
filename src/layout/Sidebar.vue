f765a0ec722525c7083a13aaf371036d8804bacf
David <hsienwu830808@gmail.com>
2019-09-17 21:30:48 +0800

    Add logo on menu

--- src/layout/Sidebar.vue
+++ src/layout/Sidebar.vue
@@ -1,28 +1,41 @@
 <template>
-  <v-navigation-drawer
-    fixed
-    clipped
-    v-model="drawer"
-    dark
-    app
-  >
-    <v-list dense>
-      <v-list-tile
-        v-for="item in items"
-        :key="item.text"
-        :to="item.path"
-      >
-        <v-list-tile-action>
-          <v-icon> {{ item.icon }}</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>
-            {{ item.text }}
-          </v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
+  <div>
+    <v-navigation-drawer
+      fixed
+      clipped
+      v-model="drawer"
+      dark
+      app
+    >
+      <v-list dense>
+        <v-list-tile
+          v-for="item in items"
+          :key="item.text"
+          :to="item.path"
+        >
+          <v-list-tile-action>
+            <template v-if="item.icon==='logo'">
+              <img
+                src="../assets/logo.png"
+                alt=""
+                style="height:25px; width=25px"
+              >
+            </template>
+            <template v-else>
+              <v-icon> {{ item.icon }}</v-icon>
+            </template>
+
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ item.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+
 </template>
 
 <script>
@@ -31,7 +44,10 @@ export default {
   data () {
     return {
       drawer: null,
+      sidebarOpen: true,
+      appTitle: 'Prediction',
       items: [
+        { icon: 'logo', text: 'Prediction', path: 'dashboard' },
         { icon: 'trending_up', text: 'Most Popular', path: 'popular' },
         { icon: 'subscriptions', text: 'Subscrptions', path: 'subscriptions' }
       ]
